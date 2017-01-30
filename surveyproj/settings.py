ab4ff9c35577dec3daf9c0d39d10e992dd0e8432
Fanis <fmkalimullin@gmail.com>
2017-01-30 23:38:40 +0300

    fixx

--- surveyproj/settings.py
+++ surveyproj/settings.py
@@ -126,3 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
