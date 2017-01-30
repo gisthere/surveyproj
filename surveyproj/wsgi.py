d77cf87c146460496371aa311357cd26d3a93e2d
Fanis <fmkalimullin@gmail.com>
2017-01-30 23:49:45 +0300

    fix again

--- surveyproj/wsgi.py
+++ surveyproj/wsgi.py
@@ -7,11 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-# import os
-#
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "surveyproj.settings")
+
 # from django.core.wsgi import get_wsgi_application
 #
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "surveyproj.settings")
 #
 # application = get_wsgi_application()
 
