0e35892c0c9fbb158873319457f30d301a5ba0c9
Fanis <fmkalimullin@gmail.com>
2017-01-30 23:00:41 +0300

    init

---
+++ surveyproj/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'surveyproj.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+# questionnaire urls
+    url(r'q/', include('questionnaire.urls')),
+
+    url(r'^take/(?P<questionnaire_id>[0-9]+)/$', 'questionnaire.views.generate_run'),
+    url(r'^$', 'questionnaire.page.views.page', {'page_to_render' : 'index'}),
+    url(r'^(?P<lang>..)/(?P<page_to_trans>.*)\.html$', 'questionnaire.page.views.langpage'),
+    url(r'^(?P<page_to_render>.*)\.html$', 'questionnaire.page.views.page'),
+    url(r'^setlang/$', 'questionnaire.views.set_language'),
+]
