ff7d0125ccb7e586e31b5b9b0581558f6c001740
Fanis <fmkalimullin@gmail.com>
2017-01-30 23:46:20 +0300

    whitenoise

--- surveyproj/wsgi.py
+++ surveyproj/wsgi.py
@@ -7,16 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "surveyproj.settings")
-
-application = get_wsgi_application()
-
+# import os
+#
 # from django.core.wsgi import get_wsgi_application
-# from whitenoise.django import DjangoWhiteNoise
+#
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "surveyproj.settings")
 #
 # application = get_wsgi_application()
-# application = DjangoWhiteNoise(application)
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
