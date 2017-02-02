070d69006e5c8c97468e3af1f51a6fbdd4d924b3
Fanis <fmkalimullin@gmail.com>
2017-02-02 21:42:56 +0300

    test

--- surveyproj/settings.py
+++ surveyproj/settings.py
@@ -91,7 +91,7 @@ DATABASES['default'].update(db_from_env)
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'UTC'
 
