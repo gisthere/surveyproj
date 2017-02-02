b6f7788ba930acd33b03ab5312c7c72695610a35
Fanis <fmkalimullin@gmail.com>
2017-02-02 21:57:47 +0300

    remove index

--- surveyproj/urls.py
+++ surveyproj/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -10,7 +11,8 @@ urlpatterns = patterns('',
     url(r'q/', include('questionnaire.urls')),
 
     url(r'^take/(?P<questionnaire_id>[0-9]+)/$', 'questionnaire.views.generate_run'),
-    url(r'^$', 'questionnaire.page.views.page', {'page_to_render' : 'index'}),
+    # url(r'^$', 'questionnaire.page.views.page', {'page_to_render' : 'index'}),
+    url(r'^$', RedirectView.as_view(url='/take/1')),
     url(r'^(?P<lang>..)/(?P<page_to_trans>.*)\.html$', 'questionnaire.page.views.langpage'),
     url(r'^(?P<page_to_render>.*)\.html$', 'questionnaire.page.views.page'),
     url(r'^setlang/$', 'questionnaire.views.set_language'),
